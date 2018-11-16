6ee308487ffa78455b00cc872c515152ffceea17
172393 <172393@student.umons.ac.be>
2018-11-16 14:26:27 +0100

    Exercice 1

--- src/main/java/exercise1/ExceptionCatcher.java
+++ src/main/java/exercise1/ExceptionCatcher.java
@@ -8,6 +8,15 @@ public class ExceptionCatcher {
 	}
 
 	void executeExceptionThrowerSafely(int value) {
-		exceptionThrower.throwACustomExceptionWhenValueIs42(value);
+		try {
+			exceptionThrower.throwACustomExceptionWhenValueIs42(value);
+		}
+		catch (RuntimeException e){
+			System.out.println("Valeur = 42");
+		}
+		finally {
+			System.out.println("Sortie de la fonction");
+		}
+
 	}
 }
